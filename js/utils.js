0e465374347420c21969685a62226fd9ad8f8c8b
moh123mos <moh162534aaa@gmail.com>
2025-07-21 02:59:07 +0300

    Implement navigation functionality for home link and enhance navigation in add product page

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+export function navigate() {
+  const origin = window.location.origin;
+  console.log(origin);
+  if (origin === 'https://ammaremad-2004.github.io/') {
+    location.href = origin + '/E_Commerce-Website/index.html';
+  } else {
+    location.href = origin + '/index.html';
+  }
+}
