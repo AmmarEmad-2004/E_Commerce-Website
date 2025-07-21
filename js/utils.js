652ef85a3157bc65cbdfd6e495bb0d1d0ef3f7f5
moh123mos <moh162534aaa@gmail.com>
2025-07-21 03:05:33 +0300

    fix

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 export function navigate() {
   const origin = window.location.origin;
   console.log(origin);
-  if (origin === 'https://ammaremad-2004.github.io/') {
+  if (origin === 'https://ammaremad-2004.github.io') {
     location.href = origin + '/E_Commerce-Website/index.html';
   } else {
     location.href = origin + '/index.html';
